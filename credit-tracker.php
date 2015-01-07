d0edaeda273986e8e183edca3eccaf5dc707b81d
R.Brown <ready.brown@hotmail.de>
2015-01-07 21:57:06 +0100

    - prepare version 1.1.0

--- credit-tracker.php
+++ credit-tracker.php
@@ -6,13 +6,13 @@
  * Description: A simple way to show credits for the images used on your website.
  * Author:      Labs64
  * Author URI:  http://www.labs64.com
- * Version:     1.0.3
+ * Version:     1.1.0
  * Text Domain: credit-tracker
  * Domain Path: /languages
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Requires at least: 3.5.1
- * Tested up to: 4.0
+ * Tested up to: 4.1
  *
  * @package   Credit_Tracker
  * @author    Labs64 <info@labs64.com>
@@ -30,7 +30,7 @@ if (!defined('WPINC')) {
 /**
  * Plugin version, used for cache-busting of style and script file references.
  */
-define('CT_VERSION', '1.0.3');
+define('CT_VERSION', '1.1.0');
 
 /**
  * Unique identifier for your plugin.
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: labs64
 Tags: credit, attribution, legal, copyright, owner, author, media library, media, image, photo, license, royalty-free, RF, Creative Commons, stock, attachment, flickr, fotolia, bildnachweis, impressum, imprint, microdata, NetLicensing
 Requires at least: 3.5.1
-Tested up to: 4.0
-Stable tag: 1.0.3
+Tested up to: 4.1
+Stable tag: 1.1.0
 License: GPL-2.0+
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -138,7 +138,7 @@ Yes you can! Join in on our [GitHub repository](https://github.com/Labs64/credit
 
 == Changelog ==
 
-= 1.0.3 =
+= 1.1.0 =
 * TODO
 
 = 1.0.2 =
