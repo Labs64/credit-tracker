92aebe56a52f6662715f2514e58d3d526122e346
R.Brown <ready.brown@hotmail.de>
2015-06-15 21:38:34 +0200

    - prepare version 1.1.2

--- credit-tracker.php
+++ credit-tracker.php
@@ -6,7 +6,7 @@
  * Description: A simple way to show credits for the images used on your website.
  * Author:      Labs64
  * Author URI:  http://www.labs64.com
- * Version:     1.1.1
+ * Version:     1.1.2
  * Text Domain: credit-tracker
  * Domain Path: /languages
  * License:     GPL-2.0+
@@ -30,7 +30,7 @@ if (!defined('WPINC')) {
 /**
  * Plugin version, used for cache-busting of style and script file references.
  */
-define('CT_VERSION', '1.1.1');
+define('CT_VERSION', '1.1.2');
 
 /**
  * Unique identifier for your plugin.
